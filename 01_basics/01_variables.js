30d27451706d11798d628870229f8472ca41d6f6
Saquib Alam <95972410+saquibalam09@users.noreply.github.com>
2024-02-04 19:50:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=3433;
+let accountEmail="saquib@gmail.com";
+var accountPassword="940340";
+accountcity="Kolkata"
+let x;
+//accountId=4; //not allowed
+/* prefer not to use var
+because of issue in block scope and 
+ */
+console.table([accountId,accountEmail,accountPassword,accountcity,x]);
